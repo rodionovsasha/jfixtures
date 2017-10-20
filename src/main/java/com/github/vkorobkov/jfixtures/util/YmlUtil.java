acf1937e71ec59b6a858c1ca16654066ab41f2ea
Rodionov Alexander <allrod@mail.ru>
2017-10-20 14:27:28 +0300

    added hasYamlTwin

--- src/main/java/com/github/vkorobkov/jfixtures/util/YmlUtil.java
+++ src/main/java/com/github/vkorobkov/jfixtures/util/YmlUtil.java
@@ -1,20 +1,42 @@
 package com.github.vkorobkov.jfixtures.util;
 
+import lombok.val;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public final class YmlUtil {
+    private static final String YML_EXT = ".yml";
+    private static final String YAML_EXT = ".yaml";
+
     private YmlUtil() {
     }
 
     @SuppressWarnings("unchecked")
     public static Map<String, Object> load(Path file) throws IOException {
         Object loaded = new Yaml().load(Files.newInputStream(file));
-        return loaded == null ? Collections.emptyMap() : (Map<String, Object>)loaded;
+        return loaded == null ? Collections.emptyMap() : (Map<String, Object>) loaded;
+    }
+
+    public static boolean hasYamlTwin(Path yamlFilePath) {
+        val fullName = yamlFilePath.toString();
+        if (!fullName.endsWith(YAML_EXT) && !fullName.endsWith(YML_EXT)) {
+            throw new IllegalArgumentException("Neither " + YAML_EXT + " nor " + YML_EXT + " file extension.");
+        }
+
+        return Stream.of(YML_EXT, YAML_EXT)
+                .map(ext -> cutOffExtension(fullName) + ext)
+                .filter(name -> new File(name).exists())
+                .count() == 2;
+    }
+
+    private static String cutOffExtension(String fullName) {
+        return fullName.substring(0, fullName.lastIndexOf('.'));
     }
 }
--- src/test/groovy/com/github/vkorobkov/jfixtures/util/YmlUtilTest.groovy
+++ src/test/groovy/com/github/vkorobkov/jfixtures/util/YmlUtilTest.groovy
@@ -39,6 +39,42 @@ class YmlUtilTest extends Specification implements YamlVirtualFolder {
         yaml.isEmpty()
     }
 
+    def "should not have yml twin"() {
+        expect:
+        !YmlUtil.hasYamlTwin(testFolderPath.resolve("simple.yml"))
+    }
+
+    def "should not have yaml twin"() {
+        expect:
+        !YmlUtil.hasYamlTwin(testFolderPath.resolve("yaml_config.yaml"))
+    }
+
+    def "should have yaml twin"() {
+        setup:
+        def folder = unpackYamlToTempFolder("twins.yml")
+
+        expect:
+        YmlUtil.hasYamlTwin(folder.resolve("simple.yml"))
+
+        cleanup:
+        folder.toFile().deleteDir()
+    }
+
+    def "hasYamlTwin throws when neither yaml nor yml extension"() {
+        setup:
+        def folder = unpackYamlToTempFolder("txt.yml")
+
+        when:
+        YmlUtil.hasYamlTwin(folder.resolve("simple.txt"))
+
+        then:
+        def e = thrown(IllegalArgumentException)
+        e.message == "Neither .yaml nor .yml file extension."
+
+        cleanup:
+        folder.toFile().deleteDir()
+    }
+
     private load(String ymlFile) {
         YmlUtil.load(testFolderPath.resolve(ymlFile))
     }
