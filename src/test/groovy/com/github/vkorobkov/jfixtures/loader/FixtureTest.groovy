935b1b67e35a1d068071bc56314721a4f13932a8
vlad <vladimir.korobkov@gmail.com>
2018-03-12 21:04:18 +0300

    API for merging fixture rows

--- src/test/groovy/com/github/vkorobkov/jfixtures/loader/FixtureTest.groovy
+++ src/test/groovy/com/github/vkorobkov/jfixtures/loader/FixtureTest.groovy
@@ -39,7 +39,6 @@ class FixtureTest extends Specification {
         thrown(UnsupportedOperationException)
     }
 
-
     def "rows returns the same collection every call"() {
         when:
         def fixture = new Fixture("users", rows)
@@ -47,5 +46,78 @@ class FixtureTest extends Specification {
         then:
         fixture.name == "users"
         fixture.rows.is(fixture.rows)
+    }
+
+    def "#mergeRows adds new rows to the end"() {
+        given:
+        def fixture = new Fixture("users",
+            [new FixtureRow("Vlad", [age: 30]), new FixtureRow("Mr Burns", [age: 100])]
+        )
+
+        when:
+        def merged = fixture.mergeRows([
+            new FixtureRow("Homer", [age: 50]),
+            new FixtureRow("Bart", [age: 12])
+        ])
+
+        then:
+        merged.rows.asList() == [
+            new FixtureRow("Vlad", [age: 30]),
+            new FixtureRow("Mr Burns", [age: 100]),
+            new FixtureRow("Homer", [age: 50]),
+            new FixtureRow("Bart", [age: 12])
+        ]
+    }
+
+    def "#mergeRows replaces rows with the same name"() {
+        given:
+        def fixture = new Fixture("users",
+            [new FixtureRow("Vlad", [age: 29]), new FixtureRow("Mr Burns", [age: 100])]
+        )
+
+        when:
+        def merged = fixture.mergeRows([
+            new FixtureRow("Homer", [age: 50]),
+            new FixtureRow("Vlad", [age: 30, skill: "java"])
+        ])
+
+        then:
+        merged.rows.asList() == [
+            new FixtureRow("Vlad", [age: 30, skill: "java"]),
+            new FixtureRow("Mr Burns", [age: 100]),
+            new FixtureRow("Homer", [age: 50])
+        ]
+    }
+
+    def "#mergeRows returns original rows if rows to merge is an empty list"() {
+        given:
+        def fixture = new Fixture("users",
+            [new FixtureRow("Vlad", [age: 30]), new FixtureRow("Mr Burns", [age: 100])]
+        )
+
+        when:
+        def merged = fixture.mergeRows([])
+
+        then:
+        merged.rows.asList() == [
+            new FixtureRow("Vlad", [age: 30]),
+            new FixtureRow("Mr Burns", [age: 100])
+        ]
+    }
+
+    def "#mergeRows returns fixture with original name"() {
+        given:
+        def fixture = new Fixture("users",
+            [new FixtureRow("Vlad", [age: 29]), new FixtureRow("Mr Burns", [age: 100])]
+        )
+
+        when:
+        def merged = fixture.mergeRows([
+            new FixtureRow("Homer", [age: 50]),
+            new FixtureRow("Vlad", [age: 30, skill: "java"])
+        ])
+
+        then:
+        merged.name == "users"
     }
 }
