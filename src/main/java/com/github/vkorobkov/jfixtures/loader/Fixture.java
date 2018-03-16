18ae72bc0915f507a1bb2680fa9cb4355cab3cc8
Vlad <vkorobkov@users.noreply.github.com>
2018-03-16 10:46:15 +0300

    API for merging fixture rows

--- src/main/java/com/github/vkorobkov/jfixtures/loader/Fixture.java
+++ src/main/java/com/github/vkorobkov/jfixtures/loader/Fixture.java
@@ -1,9 +1,13 @@
 package com.github.vkorobkov.jfixtures.loader;
 
 import lombok.Getter;
+import lombok.val;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Fixture {
     public final String name;
@@ -14,5 +18,12 @@ public final class Fixture {
     public Fixture(String name, Collection<FixtureRow> rows) {
         this.name = name;
         this.rows = Collections.unmodifiableCollection(rows);
+    }
+
+    public Fixture mergeRows(Collection<FixtureRow> toMerge) {
+        val mergedRows = Stream.concat(this.rows.stream(), toMerge.stream()).collect(
+            Collectors.toMap(FixtureRow::getName, row -> row, (oldRow, newRow) -> newRow, LinkedHashMap::new)
+        ).values();
+        return new Fixture(this.name, mergedRows);
     }
 }
