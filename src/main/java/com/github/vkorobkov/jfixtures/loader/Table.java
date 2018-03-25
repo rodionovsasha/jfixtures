bb1b3cf5e45c199952bc91768e39369d4ee83da7
Vlad <vkorobkov@users.noreply.github.com>
2018-03-25 14:43:55 +0300

    Renaming fixture to table

--- src/main/java/com/github/vkorobkov/jfixtures/loader/Table.java
+++ src/main/java/com/github/vkorobkov/jfixtures/loader/Table.java
@@ -9,30 +9,30 @@ import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public final class Fixture {
+public final class Table {
     public final String name;
 
     @Getter
     private final Collection<Row> rows;
 
-    public Fixture(String name, Collection<Row> rows) {
+    public Table(String name, Collection<Row> rows) {
         this.name = name;
         this.rows = Collections.unmodifiableCollection(rows);
     }
 
-    public static Collection<Fixture> mergeFixtures(Collection<Fixture> toMerge) {
+    public static Collection<Table> mergeTables(Collection<Table> toMerge) {
         return toMerge.stream().collect(Collectors.toMap(
-            fixture -> fixture.name, // key
-            fixture -> fixture, // value
-            (oldFixture, newFixture) -> oldFixture.mergeRows(newFixture.rows), // merge rows on conflict
+            table -> table.name, // key
+            table -> table, // value
+            (oldTable, newTable) -> oldTable.mergeRows(newTable.rows), // merge rows on conflict
             LinkedHashMap::new // keep order
         )).values();
     }
 
-    public Fixture mergeRows(Collection<Row> toMerge) {
+    public Table mergeRows(Collection<Row> toMerge) {
         val mergedRows = Stream.concat(this.rows.stream(), toMerge.stream()).collect(
             Collectors.toMap(Row::getName, row -> row, (oldRow, newRow) -> newRow, LinkedHashMap::new)
         ).values();
-        return new Fixture(this.name, mergedRows);
+        return new Table(this.name, mergedRows);
     }
 }
--- src/test/groovy/com/github/vkorobkov/jfixtures/loader/TableTest.groovy
+++ src/test/groovy/com/github/vkorobkov/jfixtures/loader/TableTest.groovy
@@ -2,7 +2,7 @@ package com.github.vkorobkov.jfixtures.loader
 
 import spock.lang.Specification
 
-class FixtureTest extends Specification {
+class TableTest extends Specification {
     Collection<Row> rows
 
     void setup() {
@@ -15,7 +15,7 @@ class FixtureTest extends Specification {
 
     def "dummy constructor test"() {
         when:
-        def fixture = new Fixture("users", [])
+        def fixture = new Table("users", [])
 
         then:
         fixture.name == "users"
@@ -24,7 +24,7 @@ class FixtureTest extends Specification {
 
     def "returns rows provided by constructor"() {
         when:
-        def fixture = new Fixture("users", rows)
+        def fixture = new Table("users", rows)
 
         then:
         fixture.rows.asList() == rows
@@ -32,7 +32,7 @@ class FixtureTest extends Specification {
 
     def "rows is a read only collection"() {
         when:
-        def fixture = new Fixture("users", rows)
+        def fixture = new Table("users", rows)
         fixture.rows.remove("Vlad")
 
         then:
@@ -41,7 +41,7 @@ class FixtureTest extends Specification {
 
     def "rows returns the same collection every call"() {
         when:
-        def fixture = new Fixture("users", rows)
+        def fixture = new Table("users", rows)
 
         then:
         fixture.name == "users"
@@ -50,7 +50,7 @@ class FixtureTest extends Specification {
 
     def "#mergeRows adds new rows to the end"() {
         given:
-        def fixture = new Fixture("users",
+        def fixture = new Table("users",
             [new Row("Vlad", [age: 30]), new Row("Mr Burns", [age: 100])]
         )
 
@@ -71,7 +71,7 @@ class FixtureTest extends Specification {
 
     def "#mergeRows replaces rows with the same name"() {
         given:
-        def fixture = new Fixture("users",
+        def fixture = new Table("users",
             [new Row("Vlad", [age: 29]), new Row("Mr Burns", [age: 100])]
         )
 
@@ -91,7 +91,7 @@ class FixtureTest extends Specification {
 
     def "#mergeRows returns original rows if rows to merge is an empty list"() {
         given:
-        def fixture = new Fixture("users",
+        def fixture = new Table("users",
             [new Row("Vlad", [age: 30]), new Row("Mr Burns", [age: 100])]
         )
 
@@ -107,7 +107,7 @@ class FixtureTest extends Specification {
 
     def "#mergeRows returns fixture with original name"() {
         given:
-        def fixture = new Fixture("users",
+        def fixture = new Table("users",
             [new Row("Vlad", [age: 29]), new Row("Mr Burns", [age: 100])]
         )
 
@@ -121,19 +121,19 @@ class FixtureTest extends Specification {
         merged.name == "users"
     }
 
-    def "#mergeFixtures puts fixtures for different tables in sequence"() {
+    def "#mergeTables puts fixtures for different tables in sequence"() {
         given:
-        def users = new Fixture("users", [
+        def users = new Table("users", [
                 new Row("Vlad",  [name: "Vlad", age: 30]),
                 new Row("Burns", [name: "Mr Burns", age: 130])
         ])
-        def roles = new Fixture("roles", [
+        def roles = new Table("roles", [
                 new Row("user",  [type: "user", readAccess: true, writeAccess: false]),
                 new Row("admin", [type: "admin", readAccess: true, writeAccess: true])
         ])
 
         when:
-        def merged = Fixture.mergeFixtures([users, roles])
+        def merged = Table.mergeTables([users, roles])
 
         then:
         merged.size() == 2
@@ -143,19 +143,19 @@ class FixtureTest extends Specification {
         merged[1] == roles
     }
 
-    def "#mergeFixtures concatenates different rows of the same table"() {
+    def "#mergeTables concatenates different rows of the same table"() {
         given:
-        def users1 = new Fixture("users", [
+        def users1 = new Table("users", [
                 new Row("Vlad",  [name: "Vlad", age: 30]),
                 new Row("Burns", [name: "Mr Burns", age: 130])
         ])
-        def users2 = new Fixture("users", [
+        def users2 = new Table("users", [
                 new Row("Homer", [name: "Homer", age: 40]),
                 new Row("Bart", [name: "Bart", age: 12])
         ])
 
         when:
-        def merged = Fixture.mergeFixtures([users1, users2])
+        def merged = Table.mergeTables([users1, users2])
 
         then:
         merged.size() == 1
@@ -167,18 +167,18 @@ class FixtureTest extends Specification {
         }
     }
 
-    def "#mergeFixtures replaces old rows with new ones in scope of the same table"() {
+    def "#mergeTables replaces old rows with new ones in scope of the same table"() {
         given:
-        def users1 = new Fixture("users", [
+        def users1 = new Table("users", [
                 new Row("Vlad",  [name: "Vlad", age: 29]),
                 new Row("Burns", [name: "Mr Burns", age: 130])
         ])
-        def users2 = new Fixture("users", [
+        def users2 = new Table("users", [
             new Row("Vlad", [name: "Vladimir", age: 30])
         ])
 
         when:
-        def merged = Fixture.mergeFixtures([users1, users2])
+        def merged = Table.mergeTables([users1, users2])
 
         then:
         merged.size() == 1
