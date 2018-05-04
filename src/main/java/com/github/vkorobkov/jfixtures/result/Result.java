bd5bb4806991d4c7ad11950a2ee9dce9a3a1706e
Vlad <vkorobkov@users.noreply.github.com>
2018-05-04 14:02:49 +0300

    API for returning rough instructions and for applying a visitor

--- src/main/java/com/github/vkorobkov/jfixtures/result/Result.java
+++ src/main/java/com/github/vkorobkov/jfixtures/result/Result.java
@@ -1,6 +1,7 @@
 package com.github.vkorobkov.jfixtures.result;
 
 import com.github.vkorobkov.jfixtures.instructions.Instruction;
+import com.github.vkorobkov.jfixtures.instructions.InstructionVisitor;
 import lombok.Getter;
 
 import java.util.Collection;
@@ -12,5 +13,9 @@ public class Result {
 
     public Result(Collection<Instruction> instructions) {
         this.instructions = Collections.unmodifiableCollection(instructions);
+    }
+
+    public void visit(InstructionVisitor visitor) {
+        instructions.forEach(instruction -> instruction.accept(visitor));
     }
 }
